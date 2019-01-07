78d327bb79b48a06178930e8e9d8ddec8cb43938
Locutus73 <alessandro.miele@gmail.com>
2019-01-07 21:06:00 +0100

    Version 1.0

--- vsync_match_display.sh
+++ vsync_match_display.sh
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Copyright 2018-2019 Alessandro "Locutus73" Miele
+# Copyright 2019 Alessandro "Locutus73" Miele
 
 # You can download the latest version of this script from:
 # https://github.com/MiSTer-devel/Scripts_MiSTer
