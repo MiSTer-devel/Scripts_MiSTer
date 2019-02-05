f5b9be0420722ddb5b022a4b542ed7e2f33d9084
Locutus73 <alessandro.miele@gmail.com>
2019-02-05 15:59:57 +0100

    Version 1.0
    First commit

---
+++ gdrive_config_download.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2019 Alessandro "Locutus73" Miele
+
+# You can download the latest version of this script from:
+# https://github.com/MiSTer-devel/Scripts_MiSTer
+
+# Version 1.0 - 2019-02-XX - First commit
+
+
+
+SCRIPT_PATH="$(realpath "$0")"
+
+GDRIVE_URL="https://github.com/odeke-em/drive"
+GDRIVE_COMMAND="gdrive"
+GDRIVE_OPTIONS="pull -exclude-ops delete -ignore-conflict -no-prompt"
+BASE_PATH="/media/fat"
+SYNC_PATH="$BASE_PATH/config"
+
+source "$(dirname "$SCRIPT_PATH")/gdrive.sh.inc"
---
+++ rclone_config_downoad.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2019 Alessandro "Locutus73" Miele
+
+# You can download the latest version of this script from:
+# https://github.com/MiSTer-devel/Scripts_MiSTer
+
+# Version 1.0 - 2019-02-05 - First commit
+
+
+
+SCRIPT_PATH="$(realpath "$0")"
+
+RCLONE_URL="https://downloads.rclone.org/rclone-current-linux-arm.zip"
+RCLONE_CONFIG="$(dirname "$SCRIPT_PATH")/rclone.conf"
+RCLONE_OPTIONS="--verbose"
+RCLONE_COMMAND="copy"
+RCLONE_SD_DIR="config"
+RCLONE_SOURCE="MiSTer:MiSTer/$RCLONE_SD_DIR"
+RCLONE_DEST="/media/fat/$RCLONE_SD_DIR"
+
+source "$(dirname "$SCRIPT_PATH")/rclone.sh.inc"
