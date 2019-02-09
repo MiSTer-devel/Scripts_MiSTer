02fb76af168cfcf077610950bf47545fc14e2ac1
Locutus73 <alessandro.miele@gmail.com>
2019-02-09 16:38:18 +0100

    Version 1.0
    First commit.

---
+++ video_mode_ntsc_pal_off.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2019 Alessandro "Locutus73" Miele
+
+# You can download the latest version of this script from:
+# https://github.com/MiSTer-devel/Scripts_MiSTer
+
+# Version 1.0 - 2019-02-09 - First commit.
+
+PROPERTY_NAME="video_mode_ntsc_pal"
+PROPERTY_VALUE="0"
+source "$(dirname $(readlink -f $0))/change_ini_property.sh.inc"
