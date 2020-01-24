43b3eadb660460ee7539b9c3c013be9a0296db4d
Paul Sajna <sajattack@gmail.com>
2020-01-23 23:25:49 -0800

    fix logic

--- other_authors/Copy-Mame-Roms.sh
+++ other_authors/Copy-Mame-Roms.sh
@@ -7,13 +7,13 @@ then
     echo "Set the paths to your mame roms and MiSTer directories in Copy-Mame-Roms.ini"
     exit 1
 else
-    if [[ -n $MAME_PATH || -n $MISTER_MAME_PATH ]];
+    if [[ -n $MAME_PATH && -n $MISTER_MAME_PATH ]];
     then
         for file in `cat mame-filelist.txt`; do
             cp ${MAME_PATH}/${file} ${MISTER_MAME_PATH}
         done
     fi
-    if [[ -n $HBMAME_PATH || -n $MISTER_HBMAME_PATH ]];
+    if [[ -n $HBMAME_PATH && -n $MISTER_HBMAME_PATH ]];
     then
         for file in `cat hbmame-filelist.txt`; do
             cp ${HBMAME_PATH}/${file} ${MISTER_HBMAME_PATH}
