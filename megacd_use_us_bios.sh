9955b954abe7642e13641429a419bff21da8770f
Keith F. Kelly <keith.f.kelly@outlook.com>
2020-04-22 12:46:06 -0700

    Add scripts for conveniently switching MegaCD BIOSes using a gamepad

---
+++ megacd_use_us_bios.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2020 Keith F. Kelly
+
+# Version 1.0 - 2020-04-22
+
+cp -f /media/fat/MegaCD/us_scd1_9210.bin /media/fat/MegaCD/boot.rom
+exit 0
