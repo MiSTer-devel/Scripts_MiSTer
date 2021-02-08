0d7b5156ceddae960ec1215298509b7f38160206
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-02-08 22:27:43 +0100

    Better error handling and arguments added

--- other_authors/mra_rom_check.sh
+++ other_authors/mra_rom_check.sh
@@ -1,7 +1,19 @@
-#!/bin/python
+#!/usr/bin/python
+
 import os
 import xml.etree.ElementTree as ET
 import zipfile
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--mra-folder", default="/media/fat/_Arcade/")
+parser.add_argument("-f", "--file", default="")
+parser.add_argument("-ir", "--ignore-roms", action='store_true')
+parser.add_argument("-ic", "--ignore-crc", action='store_true')
+parser.add_argument("-im", "--ignore-mameversion", action='store_true')
+parser.add_argument("-r", "--recursive", action='store_true')
+args = parser.parse_args()
 
 mame_paths = [
 		"/usb0/mame",
@@ -37,7 +49,7 @@ def output_line(line):
     logfile.write(line)
     logfile.write('\n')
 
-def output_line_mameversion(line):
+def output_line_logonly(line):
     #print(line)
     logfile_v.write(line)
     logfile_v.write('\n')
@@ -63,22 +75,28 @@ def parseMRA(mraFile):
               zipfiles = zipfiles+ zip.split('|')
     #output_line(zipfiles)
     crclist = []
-    for zipfilename in zipfiles:
-      try:
-        mame_folder=find_mame_folder()
-        zf = zipfile.ZipFile(mame_folder+'/'+zipfilename)
-        for zi in zf.infolist():
-          #output_line(zi.filename)
-          #output_line('{:x}'.format(zi.CRC))
-          #output_line('{0:0{1}x}'.format(zi.CRC,8))
-          crclist.append('{0:0{1}x}'.format(zi.CRC,8))
-      except:
-          #output_line('file not found: '+zipfilename)
-          if ('filename' in info):
-            info['filename'].append(zipfilename)
-          else:
-            info['filename']=[]
-            info['filename'].append(zipfilename)
+    if not args.ignore_roms:
+      somezip = False
+      for zipfilename in zipfiles:
+        try:
+          mame_folder=find_mame_folder()
+          zf = zipfile.ZipFile(mame_folder+'/'+zipfilename)
+          for zi in zf.infolist():
+            #output_line(zi.filename)
+            #output_line('{:x}'.format(zi.CRC))
+            #output_line('{0:0{1}x}'.format(zi.CRC,8))
+            crclist.append('{0:0{1}x}'.format(zi.CRC,8))
+
+          somezip = True
+        except:
+            #output_line('file not found: '+zipfilename)
+            if ('zipfilenames' in info):
+              info['zipfilenames'].append(zipfilename)
+            else:
+              info['zipfilenames']=[]
+              info['zipfilenames'].append(zipfilename)
+      if not somezip and len(zipfiles) > 0:
+        working = False
 
     #output_line(crclist)
     for item in root.findall('rom/part'):
@@ -86,40 +104,64 @@ def parseMRA(mraFile):
         if ('crc' in item.attrib):
           noCRC = False
           crc=item.attrib['crc']
-          if (crc.lower() in crclist):
+          if (crc.lower() in crclist) or args.ignore_roms:
             a=1
             #output_line('rom found')
           else:
             #output_line('**ROM NOT FOUND**  '+crc)
             if (crc in info):
-              info['crc'].append(crc)
-              info['name'].append(item.attrib['name'])
+              info['partcrcs'].append(crc)
+              info['partnames'].append(item.attrib['name'])
             else:
-              info['crc']=[]
-              info['name']=[]
-              info['crc'].append(crc)
-              info['name'].append(item.attrib['name'])
+              info['partcrcs']=[]
+              info['partnames']=[]
+              info['partcrcs'].append(crc)
+              info['partnames'].append(item.attrib['name'])
             working = False
+    if noCRC and len(zipfiles) and not args.ignore_crc:
+      info['badcrcs']=':NO CRC, Could not validate'
+      output_line_logonly(mraFile+info['badcrcs'])
+      working = False
+
+    if noMameVersion and not args.ignore_mameversion:
+      info['badmameversion']=':No MameVersion'
+      output_line_logonly(mraFile+info['badmameversion'])
+      working = False
+
     if not working:
       broken.append(info)
-    if noCRC and len(zipfiles):
-      output_line(mraFile+':NO CRC, Could not validate')
-    if noMameVersion:
-      output_line_mameversion(mraFile+':No MameVersion ')
+
 
     return working
 
 def iterateMRAFiles(directory):
+    total_mras = 0
+    passing_mras = 0
     for filename in os.listdir(directory):
-        if filename.endswith(".mra"):
-            fullname=os.path.join(directory,filename)
+        fullname = os.path.join(directory, filename)
+        if os.path.islink(fullname):
+          continue
+        elif os.path.isdir(fullname) and args.recursive:
+          totals = iterateMRAFiles(fullname)
+          total_mras = total_mras + totals[0]
+          passing_mras = passing_mras + totals[1]
+        elif filename.lower().endswith(".mra"):
             #output_line(fullname)
             try:
               working=parseMRA(fullname)
-            except:
-              output_line('Broken XML:'+fullname)
+              if working:
+                  passing_mras = passing_mras + 1
+            except Exception as e:
+              info = {}
+              info['brokenxml'] = str(e)
+              info['mraname'] = fullname
+              broken.append(info)
+              
+            total_mras = total_mras + 1
             #if not working:
             #    output_line('Not Working:'+fullname)
+
+    return [total_mras, passing_mras]
             
 #########################################
 # Create Logs subdirectory for log output
@@ -142,28 +184,45 @@ else:
 logfile = open("Logs/mra_rom_check.log", "w")
 logfile_v = open("Logs/mra_rom_check_mamever.log", "w")
 
-output_line("checking /media/fat/_Arcade/")
-#logfile.write("checking /media/fat/_Arcade/")
-iterateMRAFiles('/media/fat/_Arcade/')
+if args.file != "":
+  output_line("checking " + args.file)
+  #logfile.write("checking " + args.file)
+  working=parseMRA(args.file)
+  if working:
+      output_line("OK")
+  else:
+      output_line("Error")
+else:
+  output_line("checking " + args.mra_folder)
+  #logfile.write("checking " + args.mra_folder)
+  totals = iterateMRAFiles(args.mra_folder)
+  print ("Total MRAs processed: " + str(totals[0]))
+  print ("MRAs passing: " + str(totals[1]))
 
 for info in broken:
     #print(info)
     missingzips=""
     wrongcrc=""
-    if ('filename' in info):
-      for fname in info['filename']:
-        missingzips=missingzips+fname+","
-    if ('name' in info):
-      for name in info['name']:
+    if ('zipfilenames' in info):
+      for zipname in info['zipfilenames']:
+        missingzips=missingzips+zipname+","
+    if ('partnames' in info):
+      for name in info['partnames']:
         wrongcrc=wrongcrc+name+","
+
     errorstr = ""
+    if ('brokenxml') in info:
+        errorstr=errorstr+" broken XML: "+info['brokenxml']+" "
+    if ('badmameversion' in info):
+        errorstr=errorstr+" wrong mameversion: "+info['badmameversion']+" "
+    if ('badcrcs' in info):
+        errorstr=errorstr+" wrong crcs: "+info['badcrcs']+" "
     if (len(missingzips)):
         errorstr=errorstr+" missing zip: "+missingzips+" "
     if (len(wrongcrc)):
         errorstr=errorstr+" wrong crc for: "+wrongcrc+" "
 
     output_line(errorstr+" for: "+info['mraname'])
-
 
 #working=parseMRA('Xevious.mra')
 #working=parseMRA('Tapper.mra')
@@ -178,3 +237,5 @@ for info in broken:
 logfile.close()
 logfile_v.close()
 
+if len(broken) > 0:
+    exit(1)
