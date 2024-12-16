eab464f0c35ff9883156bd4f1a4f3e5cda5cd2ad
Keith F. Kelly <keith.f.kelly@outlook.com>
2024-12-16 15:47:53 -0800

    FEATURE: Extract script to persistently set reasonable sshd timeouts

--- make_linux_terminal_nicer.sh
+++ make_linux_terminal_nicer.sh
@@ -17,9 +17,6 @@ cp -f "$THIS_DIR/nice-linux/.profile" /root/
 cp -f "$THIS_DIR/nice-linux/.vimrc" /root/
 cp -rf "$THIS_DIR/nice-linux/.ssh" /root/
 
-echo " - Configuring ssh KeepAlive settings..."
-sed -i -E 's|^#[[:blank:]]*ClientAliveInterval[[:blank:]]*.*$|ClientAliveInterval 60|g; s|^#[[:blank:]]*ClientAliveCountMax[[:blank:]]*.*$|ClientAliveCountMax 10|g' /etc/ssh/sshd_config
-
 if [ -z "$1" ]; then
     "$THIS_DIR/add_to_user_startup.sh" "$0" "Make Linux terminal nicer"
 fi
