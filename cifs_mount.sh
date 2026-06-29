1b5b6231a6bddf2266d99c405b11449ea35fb5b5
Dave Wongillies <davewongillies@users.noreply.github.com>
2026-06-28 22:46:07 -0700

    Remove cifs_common, move functions into cifs_mount & cifs_umount (#138)

--- cifs_mount.sh
+++ cifs_mount.sh
@@ -126,6 +126,49 @@ BOOT_ARG="--boot-start"
 USER_STARTUP="/media/fat/linux/user-startup.sh"
 USER_STARTUP_TEMPLATE="/media/fat/linux/_user-startup.sh"
 
+trim_ini_field() {
+	local FIELD="$1"
+	FIELD="${FIELD#"${FIELD%%[!$' \t']*}"}"
+	FIELD="${FIELD%"${FIELD##*[!$' \t']}"}"
+	printf '%s' "$FIELD"
+}
+
+load_cifs_ini() {
+	local INI_FILE="$1"
+	local LINE KEY VALUE ALLOWED_KEY KEY_ALLOWED
+	shift
+
+	while IFS= read -r LINE || [ "$LINE" != "" ]
+	do
+		LINE=${LINE%$'\r'}
+		case "$LINE" in
+			*=*) ;;
+			*) continue ;;
+		esac
+		KEY=$(trim_ini_field "${LINE%%=*}")
+		case "$KEY" in
+			''|\#*) continue ;;
+		esac
+
+		KEY_ALLOWED="false"
+		for ALLOWED_KEY in "$@"
+		do
+			if [ "$KEY" == "$ALLOWED_KEY" ]
+			then
+				KEY_ALLOWED="true"
+				break
+			fi
+		done
+		[ "$KEY_ALLOWED" == "true" ] || continue
+
+		VALUE=$(trim_ini_field "${LINE#*=}")
+		case "$VALUE" in
+			\"*) VALUE=${VALUE#\"}; VALUE=${VALUE%%\"*} ;;
+			\'*) VALUE=${VALUE#\'}; VALUE=${VALUE%%\'*} ;;
+		esac
+		printf -v "$KEY" '%s' "$VALUE"
+	done < "$INI_FILE"
+}
 resolve_path() {
 	if command -v realpath >/dev/null 2>&1
 	then
@@ -146,11 +189,6 @@ fi
 SCRIPT_NAME=${ORIGINAL_SCRIPT_PATH##*/}
 SCRIPT_NAME=${SCRIPT_NAME%.*}
 SCRIPT_REALPATH=$(resolve_path "$ORIGINAL_SCRIPT_PATH")
-case "$SCRIPT_REALPATH" in
-	*/*) SCRIPT_DIR=${SCRIPT_REALPATH%/*} ;;
-	*) SCRIPT_DIR="." ;;
-esac
-. "$SCRIPT_DIR/cifs_common.sh"
 CIFS_MOUNT_INI_KEYS=(
 	SERVER
 	SHARE
--- cifs_umount.sh
+++ cifs_umount.sh
@@ -34,7 +34,49 @@ SINGLE_CIFS_CONNECTION="true"
 SPECIAL_DIRECTORIES="config|linux|System Volume Information"
 LAZY_UNMOUNT_ON_BUSY="true"
 
+trim_ini_field() {
+	local FIELD="$1"
+	FIELD="${FIELD#"${FIELD%%[!$' \t']*}"}"
+	FIELD="${FIELD%"${FIELD##*[!$' \t']}"}"
+	printf '%s' "$FIELD"
+}
 
+load_cifs_ini() {
+	local INI_FILE="$1"
+	local LINE KEY VALUE ALLOWED_KEY KEY_ALLOWED
+	shift
+
+	while IFS= read -r LINE || [ "$LINE" != "" ]
+	do
+		LINE=${LINE%$'\r'}
+		case "$LINE" in
+			*=*) ;;
+			*) continue ;;
+		esac
+		KEY=$(trim_ini_field "${LINE%%=*}")
+		case "$KEY" in
+			''|\#*) continue ;;
+		esac
+
+		KEY_ALLOWED="false"
+		for ALLOWED_KEY in "$@"
+		do
+			if [ "$KEY" == "$ALLOWED_KEY" ]
+			then
+				KEY_ALLOWED="true"
+				break
+			fi
+		done
+		[ "$KEY_ALLOWED" == "true" ] || continue
+
+		VALUE=$(trim_ini_field "${LINE#*=}")
+		case "$VALUE" in
+			\"*) VALUE=${VALUE#\"}; VALUE=${VALUE%%\"*} ;;
+			\'*) VALUE=${VALUE#\'}; VALUE=${VALUE%%\'*} ;;
+		esac
+		printf -v "$KEY" '%s' "$VALUE"
+	done < "$INI_FILE"
+}
 
 resolve_path() {
 	if command -v realpath >/dev/null 2>&1
@@ -58,7 +100,6 @@ case "$SCRIPT_REALPATH" in
 	*/*) SCRIPT_DIR=${SCRIPT_REALPATH%/*} ;;
 	*) SCRIPT_DIR="." ;;
 esac
-. "$SCRIPT_DIR/cifs_common.sh"
 CIFS_UMOUNT_INI_KEYS=(
 	BASE_PATH
 	LOCAL_DIR
